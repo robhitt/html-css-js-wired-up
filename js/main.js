58059d387727484bb41a63923734c463fc0b666e
Rob Hitt <robhitt@gmail.com>
2018-03-02 09:04:22 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//   // Handler when the DOM is fully loaded
+// });
+
+window.addEventListener('load', app);
+
+function app() {
+  
+}
